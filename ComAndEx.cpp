022e53d50f2c103dd3bd5edfaf795b388aef01af
Rcchio <1462111290@qq.com>
2017-04-17 07:17:49 +0800

    Update ComAndEx.cpp

--- ComAndEx.cpp
+++ ComAndEx.cpp
@@ -1,9 +1,4 @@
 #include"ComAndEx.h"
-ComAndEx::ComAndEx()
-{
-	H_number = 0;
-	mark = 0;
-}
 void ComAndEx::Compress()
 {
 	string outfilename=ScanCharacter(); //扫描源文件种字符的种类及个数，返回源文件的文件名并用string类型变量保存
@@ -13,36 +8,31 @@ void ComAndEx::Compress()
 	cout << "请输入被压缩后的文件的名字:";
 	string filename;
 	cin >> filename;
-	ofstream out(filename, ios::binary);//向压缩文件中写入源文件的信息
+	ofstream out(filename, ios::binary);
 	if (!out)
 	{
-		cout << "创建压缩文件失败.." << endl << endl;
+		cout << "文件打开失败.." << endl;
 		return;
 	}
-	out << H_number<<'s';//向压缩文件中输入字符的种类数
-	for (int i = 1; i <= H_number; ++i)//依次输入字符
+	out.write((char*)&H_number, sizeof(int));
+	for (int i = 1; i <= H_number; ++i)//依次输入字符及字符的频度
 	{
-		out << Huffmancode[i].name;
+		out.write((char*)&(HuffmanTree[i].name), sizeof(char));
+		out.write((char*)&(HuffmanTree[i].weight), sizeof(int));
 	}
-	for (int i = 1; i <= H_number; ++i)//依次输入字符对应的频度
-	{
-		out << HuffmanTree[i].weight<<'|';
-	}
-	out << 'f';
 	ifstream in(outfilename, ios::binary);//第二遍扫描源文件
 	if (!in)
 	{
 		cout << "打开源文件失败" << endl<<endl;
 		return;
 	}
-	unsigned char char_temp = '\0';//用8位的无符号变量作为读写文件的单元
-	string unit;//存储哈夫曼编码，满八位就和char_temp进行|操作
+	unsigned char char_temp = '\0';//用8位的无符号变量作为读写字符的单元
+	string unit;//存储哈夫曼编码，编码的个数大于等于八位就和char_temp进行|操作
 	while (true)
 	{
-		char_temp=in.get();
-		if (in.eof()) break;
-		//in >>noskipws>>char_temp;
-		for (int i = 1; i <= H_number; ++i)
+		char_temp=in.get();//在原文件中读取一个字符
+		if (in.eof()) break;//判断是否到达文件的末尾
+		for (int i = 1; i <= H_number; ++i)//在存取哈夫曼编码的数组中找到该字符对应的编码并将编码存储到unit中
 		{
 			if (Huffmancode[i].name == char_temp)
 			{
@@ -56,7 +46,7 @@ void ComAndEx::Compress()
 				break;
 			}
 		}
-		while (unit.length() >= 8)
+		while (unit.length() >= 8)//如果unit中存储的哈夫曼编码的位数大于等于8，就将前八位通过位操作写入char_temp中
 		{
 			char_temp = '\0';
 			for (int i = 0; i < 8; ++i)
@@ -66,8 +56,8 @@ void ComAndEx::Compress()
 					char_temp = char_temp | 1;
 			}
 			auto p = unit.begin();
-			unit.erase(p, p + 8);//清空前八位
-			out << char_temp;
+			unit.erase(p, p + 8);//清空unit中的前八位编码
+			out.write((char*)&char_temp, sizeof(unsigned char));//将载有哈夫曼编码信息的字符写入到压缩文件中去
 		}
 	}
 	if (unit.length() > 0)//处理最后不足八位的哈夫曼编码
@@ -81,19 +71,27 @@ void ComAndEx::Compress()
 		}
 		for (auto i = 0; i < (8 - unit.length()); ++i)//将编码移到到高位
 			char_temp = char_temp << 1;
-		out << char_temp;
+		out.write((char*)&char_temp, sizeof(unsigned char));
 	}
-	//out << 'e';
 	in.close();
 	out.close();
-	HuffmanTree.resize(0);//将哈夫曼树的容器置为0
-	Huffmancode.resize(0);//将哈夫曼编码的容器置为0
-	H_number = 0;         //字符的种类数置为0
+	//压缩文件完成，将哈夫曼树的数组和存储哈夫曼编码的数组的内存释放掉，并将字符的种类数置为0
+	HuffmanTree.resize(0);
+	Huffmancode.resize(0);
+	H_number = 0;
 }
+
+//生成哈夫曼编码的函数
 void ComAndEx::CreateHuffmanCode()
 {
 	Huffmancode_node elem;
 	Huffmancode.push_back(elem);
+	if (H_number == 1)
+	{
+		Huffmancode.push_back(elem);
+		Huffmancode[1].name =HuffmanTree[1].name;
+		Huffmancode[1].code.push_back(1);
+	}
 	for (int i = 1; i <= H_number; ++i)
 	{
 		Huffmancode.push_back(elem);
@@ -116,22 +114,48 @@ void ComAndEx::CreateHuffmanCode()
 		}
 	}
 }
+
+//构造哈夫曼树的函数
 void ComAndEx::CreateHuffmanTree()
 {
-    mark = H_number + 1;//HuffmanTree[mark]为第一个空的节点
+	if (H_number == 1)
+	{
+		HuffmanTree[1].lchild = HuffmanTree[1].rchild = 1;
+	}
+	int mark = H_number + 1;
 	int x = 0, y = 0;
+	int min = 999999999, secmin = 0;
 	while (mark < 2 * H_number)
 	{
-		select(x, y);
+		for (int j = 1; j < mark; ++j)
+		{
+			if (HuffmanTree[j].p == 0 && HuffmanTree[j].weight<min)
+			{
+				y = x;
+				secmin = min;
+				min = HuffmanTree[j].weight;
+				x = HuffmanTree[j].Hnodeindex;
+			}
+			else if (HuffmanTree[j].p == 0 && HuffmanTree[j].weight < secmin)
+			{
+				secmin = HuffmanTree[j].weight;
+				y = HuffmanTree[j].Hnodeindex;
+			}
+			else;
+		}
 		HuffmanTree[mark].weight = HuffmanTree[x].weight + HuffmanTree[y].weight;
 		HuffmanTree[mark].lchild = x;
 		HuffmanTree[mark].rchild = y;
 		HuffmanTree[x].p = mark;
 		HuffmanTree[y].p = mark;
 		mark++;
+		x = y = 0;
+		min = 99999999999, secmin = 0;
 	}
 	HuffmanTree[2 * H_number - 1].p = 0;
 }
+
+//扫描原文件中字符的种类及频度的函数
 string ComAndEx::ScanCharacter()
 {
 	cout << "请输入要压缩的文件的文件名:";
@@ -148,7 +172,6 @@ string ComAndEx::ScanCharacter()
 	while (true)           //判断是否到达文件的末尾
 	{
 		temp = in.get();
-		//in >> noskipws >> temp;
 		if (in.eof())break;
 		temparry[temp].name = temp;
 		temparry[temp].weight++;
@@ -156,6 +179,7 @@ string ComAndEx::ScanCharacter()
 	in.close();  //关闭in与源文件的关联
 	Hnode elem;
 	HuffmanTree.push_back(elem);//使得哈夫曼有效的存储是从HuffmanTree[1]开始
+	H_number = 0;
 	for (int i = 0; i < 256; i++)//选择出频度大于0的字符存储到哈夫曼数组中
 	{
 		if (temparry[i].weight != 0)
@@ -165,7 +189,7 @@ string ComAndEx::ScanCharacter()
 			*(HuffmanTree.end()-1) = temparry[i];
 		}
 	}
-	if (HuffmanTree.size()== 2)
+	if (HuffmanTree.size()== 1)
 	{
 		cout << "您输入的文本为空文本.." << endl << endl;
 		return "NULL";
@@ -179,65 +203,7 @@ string ComAndEx::ScanCharacter()
 	{
 		HuffmanTree[i].Hnodeindex = i;
 	}
-	test2();
 	return filename;
-}
-void ComAndEx::select(int&min, int&secmin)
-{
-	Hnode temp;
-	for (int i = 1; i < mark; ++i)
-	{
-		if (HuffmanTree[i].p == 0)
-		{
-			temp = HuffmanTree[i];
-			break;
-		}
-	}
-	for (int i = 1; i < mark; ++i)
-	{
-		if (HuffmanTree[i].p == 0)
-		{
-			if (HuffmanTree[i].weight <= temp.weight)
-				temp = HuffmanTree[i];
-		}
-	}
-	min = temp.Hnodeindex;
-	for (int i = 1; i < mark; ++i)
-	{
-		if (HuffmanTree[i].p == 0 && i != min)
-		{
-			temp = HuffmanTree[i];
-			break;
-		}
-	}
-	for (int i = 1; i < mark; ++i)
-	{
-		if (HuffmanTree[i].p == 0 && i != min)
-		{
-			if (HuffmanTree[i].weight <= temp.weight)
-				temp = HuffmanTree[i];
-		}
-	}
-	secmin = temp.Hnodeindex;
-}
-void ComAndEx::test1()
-{
-	for (int i = 1; i <= H_number; ++i)
-	{
-		cout << Huffmancode[i].name << ":";
-		for (auto elem : Huffmancode[i].code)
-		{
-			cout << elem;
-		}
-			cout << endl;
-	}
-}
-void ComAndEx::test2()
-{
-	for (int i = 1; i <= H_number; ++i)
-	{
-		cout << HuffmanTree[i].name << ":" << HuffmanTree[i].weight << endl;
-	}
 }
 void ComAndEx::Extract()
 {
@@ -247,52 +213,30 @@ void ComAndEx::Extract()
 	ifstream in(filename, ios::binary);
 	if (!in)
 	{
-		cout << "没有找到该文件.." << endl<<endl;
+		cout << "文件打开失败.." << endl;
 		return;
 	}
-	unsigned char flag = '\0';
-	in >> noskipws >> H_number;
-	//flag=in.get();
-	in >> noskipws >> flag;
-	if (flag != 's')
-	{
-		cout << "没正常扫描到s,所以字符不能正确还原.." << endl<<endl;
-		return;
-	}
+	in.read((char*)&H_number,sizeof(int));//读取字符种类数
 	Hnode elem;
 	HuffmanTree.push_back(elem);
-	for (int i = 1; i <= H_number; ++i)
+	for (int i = 1; i <= H_number; ++i)//读取字符的种类及频度
 	{
 		HuffmanTree.push_back(elem);
-		//HuffmanTree[i].name=in.get();
-		in >> noskipws >> HuffmanTree[i].name;
+		in.read((char*)&(HuffmanTree[i].name), sizeof(char));
+		in.read((char*)&(HuffmanTree[i].weight), sizeof(int));
 	}
-	for (int i = 1; i <= H_number; ++i)
-	{
-		in >> noskipws >> HuffmanTree[i].weight;
-		//flag=in.get(); 
-		in >> noskipws >> flag;
-	}
-	//flag = in.get();
-	in >> noskipws >> flag;
-	if (flag != 'f')
-	{
-		cout << "源文件信息未得到正确扫描.." << endl<<endl;
-		return;
-	}
-	for(int i=1;i<=H_number-1;++i)
+	for(int i=1;i<=H_number-1;++i)//建立完整的哈夫曼数组
 		HuffmanTree.push_back(elem);
-	for (int i = 0; i < 2 * H_number; i++)
+	for (int i = 0; i < 2 * H_number; i++)//为哈夫曼数组元素建立索引
 	{
 		HuffmanTree[i].Hnodeindex = i;
 	}
-	CreateHuffmanTree();
-	unsigned  char temp_char = '\0';
-	unsigned long long Length = 0;
-	for (int i = 1; i <= H_number; ++i)
-		Length = Length + HuffmanTree[i].weight;
+	CreateHuffmanTree();//生成哈夫曼树
+	unsigned  char temp_char = '\0';//用来读取哈夫曼编码的单位
 	unsigned long long length = 0;
-	int top = HuffmanTree[2 * H_number - 1].Hnodeindex;
+	for (int i = 1; i <= H_number; ++i)//计算原文件的字符的个数
+		length = length + HuffmanTree[i].weight;
+	int top = HuffmanTree[2 * H_number - 1].Hnodeindex;//top存储的是结点的索引，初始化时根节点的索引
 	cout << "请输入解压以后的文件的名字:";
 	string extractedname;
 	cin >> extractedname;
@@ -302,29 +246,28 @@ void ComAndEx::Extract()
 		cout << "该文件打开失败.." << endl<<endl;
 		return;
 	}
-    while(true)
+    while(length)
 	{
-		//temp_char=in.get();
-		in >> noskipws >> temp_char;
-		if (in.eof())break;
+		temp_char = in.get();//
 		for (int i = 0; i<8;++i)
 		{
-			if (temp_char & 128)
-				top = HuffmanTree[top].rchild;
+			if (temp_char & 128)//如果最高位为1
+				top = HuffmanTree[top].rchild;//top存储结点的右孩子的索引
 			else
-				top = HuffmanTree[top].lchild;
-			if (top <=H_number)
+				top = HuffmanTree[top].lchild;//如果最高位为0，top存储结点的左孩子的索引
+			if (top <=H_number)//如果从根节点到达叶节点
 			{
-				out << HuffmanTree[top].name;
-				length++;
-				if (length ==Length )break;
-				top = HuffmanTree[2 * H_number - 1].Hnodeindex;
+				out <<HuffmanTree[top].name;//输出字符
+				length--;//更新原文件中未输出的字符的个数
+				if (length ==0 )break;//如果原文件中未输出的个数为0，表明原文件的字符已经全部输出，循环不需要再继续
+				top = HuffmanTree[2 * H_number - 1].Hnodeindex;//否则top复位为根节点的索引
 			}
-			temp_char = temp_char << 1;
+			temp_char = temp_char << 1;//如果没到达叶节点，那么temp_char左移一位，继续循环
 		}
 	}
 	in.close();
 	out.close();
+	//压缩文件完成，将哈夫曼树的数组和存储哈夫曼编码的数组的内存释放掉，并将字符的种类数置为0
 	HuffmanTree.resize(0);
 	Huffmancode.resize(0);
 	H_number = 0;
